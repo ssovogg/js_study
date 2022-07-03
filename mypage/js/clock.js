7a55099b2ee784d77d535c0e24a9d83240802b58
ssovogg <susuco77@gmail.com>
2022-07-03 23:23:15 +0900

    clock & layout update

---
+++ mypage/js/clock.js
@@ -0,0 +1,21 @@
+const today = document.getElementById("today");
+const day = document.getElementById("day");
+const clock = document.getElementById("clock");
+const dayKorean = ["일", "월", "화", "수", "목", "금", "토"]
+
+getToday();
+setInterval(getToday, 1000);
+
+function getToday(){
+  const date = new Date();
+  const years = date.getFullYear();
+  const month = date.getMonth();
+  const datt = date.getDate();
+  const dayy = dayKorean[Number(date.getDate())];
+  const hours = String(date.getHours()).padStart(2,"0");
+  const minutes = String(date.getMinutes()).padStart(2,"0");
+  const second = String(date.getSeconds()).padStart(2,"0");
+  console.log(dayy);
+  day.innerText = `${years}년 ${month}월 ${datt}일 ${dayy}요일`
+  clock.innerText = `${hours}:${minutes}:${second}`
+}
